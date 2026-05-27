331b1a4c1ce3f81c92e063a8422561cd071556f0
full-finger <gyh2020163@163.com>
2026-05-27 21:33:30 +0800

    fix(apperror): 使用HTTP标准常量，添加错误链支持

--- internal/apperror/error.go
+++ internal/apperror/error.go
@@ -1,21 +1,35 @@
 package apperror
 
+import "net/http"
+
 type AppError struct {
 	Code    int
 	Message string
+	Err     error
 }
 
 func (e *AppError) Error() string {
+	if e.Err != nil {
+		return e.Message + ": " + e.Err.Error()
+	}
 	return e.Message
+}
+
+func (e *AppError) Unwrap() error {
+	return e.Err
 }
 
 func New(code int, msg string) *AppError {
 	return &AppError{Code: code, Message: msg}
 }
 
-func BadRequest(msg string) *AppError   { return New(400, msg) }
-func Unauthorized(msg string) *AppError { return New(401, msg) }
-func Forbidden(msg string) *AppError    { return New(403, msg) }
-func NotFound(msg string) *AppError     { return New(404, msg) }
-func TooMany(msg string) *AppError      { return New(429, msg) }
-func Internal(msg string) *AppError     { return New(500, msg) }
+func NewWrap(code int, msg string, err error) *AppError {
+	return &AppError{Code: code, Message: msg, Err: err}
+}
+
+func BadRequest(msg string) *AppError   { return New(http.StatusBadRequest, msg) }
+func Unauthorized(msg string) *AppError { return New(http.StatusUnauthorized, msg) }
+func Forbidden(msg string) *AppError    { return New(http.StatusForbidden, msg) }
+func NotFound(msg string) *AppError     { return New(http.StatusNotFound, msg) }
+func TooMany(msg string) *AppError      { return New(http.StatusTooManyRequests, msg) }
+func Internal(msg string) *AppError     { return New(http.StatusInternalServerError, msg) }
